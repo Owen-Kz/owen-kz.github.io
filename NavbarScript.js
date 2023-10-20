ba39fa58ab1eeadd1de4e4a3da8bf171bbe0cf2a
Owen-Kz <lordkz19@gmail.com>
2023-10-20 14:49:10 +0100

    Update NavbarScript.js

--- NavbarScript.js
+++ NavbarScript.js
@@ -1,4 +1,9 @@
-alert("Trey")
+alert("Red Trey")
+// Get a reference to the "bodyContainer" element (assuming it exists in your HTML)
+const bodyContainer = document.getElementById('bodyContainer');
+
+// Set the background image using the style property
+bodyContainer.style.backgroundImage = 'url("https://dl-prod.rteappbuilder.com/74eb5d2c70abecb166a7/ckjefbove95c94qer580.png")';
 
 const participantsIcon = document.querySelector('[data-testid="participantsIcon"]')
 
