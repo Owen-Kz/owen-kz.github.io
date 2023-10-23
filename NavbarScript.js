8311842271661cd77828518ab95489ac54684e62
Owen-Kz <lordkz19@gmail.com>
2023-10-23 23:16:52 +0100

    Body tag

--- NavbarScript.js
+++ NavbarScript.js
@@ -1,6 +1,6 @@
 // alert("Red Trey")
 // Get a reference to the "bodyContainer" element (assuming it exists in your HTML)
-const bodyContainer = document.getElementById('bodyContainer');
+const bodyContainer = document.querySelector('body');
 
 // Set the background image using the style property
 bodyContainer.style.backgroundImage = 'url("https://dl-prod.rteappbuilder.com/74eb5d2c70abecb166a7/ckjefbove95c94qer580.png")';
