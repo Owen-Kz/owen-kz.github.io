b04bf885ab5cfa353d621d2174fb9f14657ea800
Owen-Kz <lordkz19@gmail.com>
2023-12-03 00:38:57 +0100

    Portfolio

---
+++ JPG/script.js
@@ -0,0 +1,12 @@
+// const imageGallery = document.getElementById('image-gallery');
+
+// const images = [];
+// const imageFiles = fs.readdirSync('.');
+
+// for (const imageFile of imageFiles) {
+//     if (imageFile.endsWith('.jpg') || imageFile.endsWith('.png') || imageFile.endsWith('.gif')) {
+//         const image = document.createElement('img');
+//         image.src = imageFile;
+//         imageGallery.appendChild(image);
+//     }
+// }
